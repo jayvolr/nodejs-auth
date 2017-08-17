72609c8d91feda9a3c30c90547f910cf1d6c3e72
Jon Love <jonlov4@gmail.com>
2017-08-17 08:24:23 -0400

    Basic server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // Application entry point
+const express = require('express');
+
+var app = express();
+
+app
+  .set('view engine', 'hjs')
+  .get('/', (req, res) => {
+    res.render('index');
+  })
+  .listen(3000, () => {
+    console.log('Server listening on port 3000...');
+  });
