112b54d768601aff6d695daed173e9d40357a29f
Jon Love <jonlov4@gmail.com>
2017-08-17 13:03:41 -0400

    login form

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ var app = express();
 
 app
   .set('view engine', 'hjs')
+  .use(express.static(__dirname + '/public'))
   .get('/', (req, res) => {
     res.render('index');
+  })
+  .post('/login', (req, res) => {
+    console.log(req.body);
   })
   .listen(3000, () => {
     console.log('Server listening on port 3000...');
