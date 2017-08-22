77100cc7b918a2a0c6ed032dc1c70836c53071b6
Jon Love <jonlov4@gmail.com>
2017-08-22 08:11:23 -0400

    sessions

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-
 // Application entry point
 const express = require('express');
+const session = require('express-session');
 
 var app = express();
 
 app
   .set('view engine', 'hjs')
+  .use(session({
+    secret: 'authentication is fun',
+    resave: false,
+    saveUninitialized: false
+  }))
   .get('/', (req, res) => {
     res.render('index');
+  })
+  .get('/session', (req, res) => {
+    res.send(req.session);
+  })
+  .get('/set', (req, res) => {
+    req.session.name = "Jon";
+    res.redirect('/session');
   })
   .listen(3000, () => {
     console.log('Server listening on port 3000...');
