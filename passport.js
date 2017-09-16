71028107ca613700adc1716f2ef17bb9fad85ad3
Jon Love <jonlov4@gmail.com>
2017-09-16 19:37:51 -0400

    Registration

--- passport.js
+++ passport.js
@@ -1,19 +1,57 @@
+const bcrypt = require('bcryptjs');
 const LocalStrategy = require('passport-local').Strategy;
 const mongo = require('./db');
 const ObjectID = require('mongodb').ObjectID;
 const passport = require('passport');
 
 passport.use(new LocalStrategy(authenticate));
+passport.use("local-register", new LocalStrategy({
+  passReqToCallback: true
+}, register))
 
 function authenticate(email, password, done) {
   mongo.db.collection('users')
-    .findOne({ email: email }, (err, user) => {
-      if (err) return done(err);
-      if (!user || password !== user.password) {
+  .findOne({ email: email }, (err, user) => {
+    if (err) return done(err);
+    if (!user) return done(null, false, { message: "Incorrect email or password." });
+
+    bcrypt.compare(password, user.password, (err, passwordMatches) => {
+      if (passwordMatches === false) {
         return done(null, false, { message: "Incorrect email or password." });
       }
-      return done(null, user);
     });
+
+    return done(null, user);
+  });
+}
+
+function register(req, email, password, done) {
+  if (req.body.password2 !== password) {
+    return done(null, false, { message: "Passwords do not match." });
+  }
+
+  mongo.db.collection('users')
+  .findOne({ email: email }, (err, user) => {
+    if (err) return done(err);
+    if (user) {
+      return done(null, false, { message: "User with that email already exists." });
+    }
+
+    bcrypt.hash(password, 8, (err, hashedPassword) => {
+      if (err) return done(err);
+      const newUser = {
+        email: email,
+        password: hashedPassword,
+        joinDate: new Date()
+      }
+
+      mongo.db.collection('users')
+      .insert(newUser, (err, result) => {
+        if (err) return done(err);
+        return done(null, result.ops[0]);
+      });
+    });
+  });
 }
 
 passport.serializeUser(function(user, done) {
--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,10 @@ router
   .post('/login', passport.authenticate('local', {
     successRedirect: '/session',
     failureRedirect: 'back'
+  }))
+  .post('/register', passport.authenticate('local-register', {
+    successRedirect: '/session',
+    failureRedirect: 'back'
   }));
 
 module.exports = router;
